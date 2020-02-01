065b1a6fa9ae5b01e93fc469247ae2a944abe074
Marcin <marcin96semkowski@gmail.com>
2020-02-01 17:19:34 +0100

    Bootstrap fix #2 sucessfully

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <div class="container">
-     <div  class="row">
-         <div class="col-3 p-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
 
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
-         </div>
-
-         <div class="col-9 pt-5">
-             <div><h1>freecodeCamp</h1></div>
-             <div class="d-flex">
-                 <div class="pr-5"><strong>153</strong></div>
-                 <div><strong>23</strong></div>
-                 <div><strong>212</strong></div>
-
-             </div>
-         </div>
-
-
-     </div>
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-
-
-  </html>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,17 +18,13 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/app.js') }}"></script>
-
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <div><img src="/svg/freeCodeCampLogo.svg" style="height: 25px; border-right: 1px solid #333" class="pr-3" /> </div>
-                    <div class="pl-3">freeCodeCamp</div>
-                    freeCodeGram
+                    {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
